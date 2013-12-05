aef9ad9bf8891a85c0ca8f814d50e61859a67a31
Eric Tan <20925931@student.uwa.edu.au>
2013-12-05 08:23:25 +0800

    Finished home page mockup

--- mockup/application.js
+++ mockup/application.js
@@ -1,4 +1,18 @@
 $(function () {
+
+	$('#navbar2').hide();
+
+	$('#nextpage').click(function() {
+		$('#navbar2').show();
+		$('#navbar1').hide();
+	});
+	
+	$('#previouspage').click(function() {
+		$('#navbar1').show();
+		$('#navbar2').hide();
+	});
+	
+	
 	$('#slider').unslider({
 		speed: 300,
 		delay: 4000,
